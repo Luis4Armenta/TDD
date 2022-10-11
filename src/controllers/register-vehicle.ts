97d8fcb32764286c03793baa9d59736dd72f605e
Luis Angel Armenta <luigiangelxd@gmail.com>
2022-10-11 16:18:14 -0500

    refactor: RegisterVehicleController implement interfaces

--- src/controllers/register-vehicle.ts
+++ src/controllers/register-vehicle.ts
@@ -1,16 +1,24 @@
+import { HttpRequest, HttpResponse } from '../interfaces/http-interface'
+
 export class RegisterVehicle {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (httpRequest.body.name === undefined) {
       return {
         statusCode: 400,
         body: new Error('error in the: name')
       }
     }
+
     if (httpRequest.body.model === undefined) {
       return {
         statusCode: 400,
         body: new Error('error in the: model')
       }
+    }
+
+    return {
+      statusCode: 200,
+      body: '<h1>All right!</h1>'
     }
   }
 }
